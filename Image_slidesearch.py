097d83155d4500edefbe30a0685e09ceff80025a
Pushkar Inamdar <Pushkar@Pushkars-MacBook-Pro.local>
2018-01-22 09:49:17 -0800

    initial analysis

---
+++ Image_slidesearch.py
@@ -0,0 +1,74 @@
+
+# coding: utf-8
+
+# In[91]:
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from PIL import Image
+import numpy as np
+
+im_age=cv2.imread('/Users/Pushkar/Downloads/ships-in-satellite-imagery/scenes/sfbay_1.png')
+im_age=cv2.cvtColor(im_age, cv2.COLOR_BGR2GRAY)
+
+height=len(im_age[0])
+width=len(im_age[1])
+
+step = 10
+AOI_coordinates = []
+
+image_array = np.array(im_age).astype('uint8')
+plt.figure(1, figsize = (15, 30))
+plt.imshow(image_array, cmap='gray')
+plt.show()
+
+
+def clipped(x, y):
+    #Area Of Interst (AOI)
+    AOI = np.arange(1*80*80).reshape(1, 80, 80)
+    for i in range(80):
+        for j in range(80):
+            AOI[0][i][j] = '''picture_tensor'''[y+i][x+j]
+    AOI = AOI.reshape([-1, 1, 80, 80])
+    AOI = AOI.transpose([0,1,2,3])
+    AOI = AOI / 255
+    sys.stdout.write('\rX_coordinate:{0} Y_coordinate:{1}  '.format(x, y))
+    return AOI
+
+
+def dist(x, y, size, coordinates):
+    result = True
+    for point in coordinates:
+        if x+size > point[0][0] and x-size < point[0][0] and y+size > point[0][1] and y-size < point[0][1]:
+            result = False
+    return result
+
+
+
+
+try:
+    for y in range(int((height-(80-step))/step)):
+        for x in range(int((width-(80-step))/step) ):
+            area = clipped(x*step, y*step)
+            result = model.predict(area)
+            if result[0][1] > 0.90 and dist(x*step,y*step, 80, coordinates):
+                AOI_coordinates.append([[x*step, y*step], result])
+                print(result)
+                plt.imshow(area[0].squeeze())
+                plt.show()
+except IndexError:
+    pass
+
+
+im = np.array(Image.open('/Users/Pushkar/Downloads/ships-in-satellite-imagery/scenes/sfbay_1.png'), dtype=np.uint8)
+
+def bounding_boxe(coordinates,im):
+    fig,ax = plt.subplots(1)
+    ax.imshow(im)
+    for v in coordinates:
+        rect = patches.Rectangle((v[0][0],v[0][1]),80,80,linewidth=1,edgecolor='r',facecolor='none')
+        ax.add_patch(rect)
+    return plt.show()
+
+bounding_boxe(AOI_coordinates,im)
+
