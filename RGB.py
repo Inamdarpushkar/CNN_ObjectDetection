4bf143598eb2f3e4e025934791a5960902ed3ed0
Pushkar Inamdar <Pushkar@Pushkars-MacBook-Pro.local>
2018-01-24 08:27:22 -0800

    RGB Model layres

---
+++ RGB.py
@@ -0,0 +1,38 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+#layers
+model = Sequential()
+
+model.add(Convolution2D(32, 3,3,border_mode='same',input_shape=(80, 80, 3)))
+model.add(Activation('relu'))
+
+model.add(Convolution2D(32, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.50))
+
+model.add(Convolution2D(32, 3, 3))
+model.add(Activation('relu'))
+
+model.add(Convolution2D(32, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.50))
+
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.50))
+
+model.add(Dense(2))
+model.add(Activation('softmax'))
+
+#compile
+model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=["accuracy"])
+
+
+hist = model.fit(X_train, y_train, batch_size=32, nb_epoch=17, verbose=1, validation_split=0.2)
+
